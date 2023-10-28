2f7d08e77d71689fcc299f236ea2ab1eb3c2f272
Hendra Anggrian <hendraanggrian@gmail.com>
2023-10-28 01:04:42 -0500

    First Ktlint stable release

--- native-app/app/build.gradle.kts
+++ native-app/app/build.gradle.kts
@@ -9,12 +9,13 @@ ktlint.version.set(libs.versions.ktlint.get())
 kotlin {
     val os = System.getProperty("os.name")
     val isArm64 = System.getProperty("os.arch") == "aarch64"
-    val nativeTarget = when {
-        os == "Mac OS X" -> if (isArm64) macosArm64("native") else macosX64("native")
-        os == "Linux" -> if (isArm64) linuxArm64("native") else linuxX64("native")
-        os.startsWith("Windows") -> mingwX64("native")
-        else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
-    }
+    val nativeTarget =
+        when {
+            os == "Mac OS X" -> if (isArm64) macosArm64("native") else macosX64("native")
+            os == "Linux" -> if (isArm64) linuxArm64("native") else linuxX64("native")
+            os.startsWith("Windows") -> mingwX64("native")
+            else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+        }
     nativeTarget.binaries.executable {
         entryPoint = "main"
     }
